b0d735d683728c1638983a0e70e8518dd3c80757
DELL <DELL@EMIRHAN>
2021-03-18 22:50:59 +0300

    Angular 2

---
+++ src/app/models/car.ts
@@ -0,0 +1,10 @@
+export interface Car{
+    carId: number;
+    categoryId: number;
+    modelYear:string;
+    dailyPrice:number;
+    description:string;
+    colorId:number;
+    brandId:number;
+    modelId:number;
+}
